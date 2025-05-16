2ac092ba366186c3ae441bd8251401f3cd413813
h254194 <h254194@stud.u-szeged.hu>
2025-05-16 23:59:35 +0200

    merfoldko 2 begin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,4 +44,9 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:33.12.0"))
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.android.gms:play-services-auth:19.0.0")
+
+    implementation("androidx.recyclerview:recyclerview:1.1.0")
+    implementation("androidx.recycleview:recyclerview-selection:1.1.0")
+    implementation("com.github.bumptech.glide:glide:3.7.0")
+    implementation("androidx.cardview:cardview:1.0.0")
 }
